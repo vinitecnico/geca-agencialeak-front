8528b7385b00a065adea02207160ba76df6c0a9f
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2018-12-19 16:50:28 -0200

    fix enterprise

---
+++ src/app/registration/classes/enterprise.class.ts
@@ -0,0 +1,23 @@
+export class Enterprise {
+    _id: string;
+    nomefantasia: string;
+    cnpj: string;
+    segmento: string;
+    atividade: string;
+    cep: string;
+    endereco: string;
+    num: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    contato_principal: string;
+    telefone: string;
+    celular: string;
+    email: string;
+    facebook: string;
+    twitter: string;
+    instagram: string;
+    datacreate: string;
+    dataUpdate: string;
+}
---
+++ src/app/registration/services/enterprise.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Class
+import { Enterprise } from '../classes/enterprise.class';
+
+// Services
+import { StartupConfigService } from '../../shared/services/startup.config.service';
+
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class EnterpriseService {
+
+    constructor(private http: HttpClient, private apiConfig: StartupConfigService, @Inject('LocalStorage') localStorage) { }
+
+    getAll(): Observable<Enterprise[]> {
+        const url = `${this.apiConfig.domain}api/empresa`;
+        return this.http.get<Enterprise[]>(url, httpOptions);
+    }
+
+    getById(_id: string): Observable<Enterprise[]> {
+        const url = `${this.apiConfig.domain}api/empresa/${_id}`;
+        return this.http.get<Enterprise[]>(url, httpOptions);
+    }
+
+    createOrUpdateEnterprise(fair: Enterprise): Observable<any> {
+        const url = `${this.apiConfig.domain}api/empresa`;
+
+        if (fair._id) {
+            return this.http.put(`${url}/${fair._id}`, fair, httpOptions);
+        } else {
+            return this.http.post(url, fair, httpOptions);
+        }
+    }
+
+    delete(_id: string): Observable<any> {
+        const url = `${this.apiConfig.domain}api/empresa/${_id}`;
+        return this.http.delete(url, httpOptions);
+    }
+}
