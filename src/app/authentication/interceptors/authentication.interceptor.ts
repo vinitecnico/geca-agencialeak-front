9c62ffd9e977d8a76ee35ba6df295fa0c20fd501
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2019-01-07 20:28:07 -0200

    fix

--- src/app/authentication/interceptors/authentication.interceptor.ts
+++ src/app/authentication/interceptors/authentication.interceptor.ts
@@ -1,53 +1,61 @@
 import { Injectable, Injector, Inject } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 
 import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
     constructor(@Inject('LocalStorage') localStorage, private router: Router) { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         const auth = localStorage.getItem('authData');
         const authObj = JSON.parse(auth);
         const isTokenValid = this.validateAuthenticationToken(authObj);
 
-        if (req.url.indexOf('api/login') > -1) {
-            return next.handle(req);
+        if (request.url.indexOf('api/login') > -1) {
+            return next.handle(request);
         }
 
         if (isTokenValid) {
-            // Clone the request to add the new header.
-            const authReq = req.clone({
-                setHeaders: {
-                    'x-access-token': authObj.token
-                }
+            const authReq = request.clone({
+                headers: request.headers.append('x-access-token', authObj.token)
             });
 
-            // headers: req.headers.set('x-access-token', authObj.token)
+            console.log('Intercepted HTTP call', authReq);
 
             // send the newly created request
             return next.handle(authReq)
-                .catch((error, caught) => {
-                    if (error.status === 401 || error.status === 403) {
-                        localStorage.removeItem('authData');
-                        this.router.navigateByUrl('/login');
-                    }
+                .pipe(
+                    catchError(error => {
+                        // checks if a url is to an admin api or not
+                        if (error.status === 401 || error.status === 403) {
+                            // attempting to refresh our token
+                            localStorage.removeItem('authData');
+                            this.router.navigateByUrl('/login');
+                        }
+                        return Observable.throw(error);
+                    }));
+            // .catch((error, caught) => {
+            //     if (error.status === 401 || error.status === 403) {
+            //         localStorage.removeItem('authData');
+            //         this.router.navigateByUrl('/login');
+            //     }
 
-                    return Observable.throw(error);
-                }) as any;
+            //     return Observable.throw(error);
+            // }) as any;
         }
 
         if (authObj != null && !isTokenValid) {
             this.router.navigateByUrl('/login');
         }
 
-        return next.handle(req);
+        return next.handle(request);
     }
 
     validateAuthenticationToken(authData): boolean {
