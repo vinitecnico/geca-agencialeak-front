21bd75f043e343af6256e15b64d47b6f736b08db
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2019-01-09 20:52:22 -0200

    fix menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,25 @@
 $.noConflict();
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
 	"use strict";
 
-	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
+	[].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
 		new SelectFx(el);
 	});
 
 	jQuery('.selectpicker').selectpicker;
 
 
-	
 
-	$('.search-trigger').on('click', function(event) {
+
+	$('.search-trigger').on('click', function (event) {
 		event.preventDefault();
 		event.stopPropagation();
 		$('.search-trigger').parent('.header-left').addClass('open');
 	});
 
-	$('.search-close').on('click', function(event) {
+	$('.search-close').on('click', function (event) {
 		event.preventDefault();
 		event.stopPropagation();
 		$('.search-trigger').parent('.header-left').removeClass('open');
@@ -35,56 +35,59 @@ jQuery(document).ready(function($) {
 
 	// Counter Number
 	$('.count').each(function () {
-		$(this).prop('Counter',0).animate({
+		$(this).prop('Counter', 0).animate({
 			Counter: $(this).text()
 		}, {
-			duration: 3000,
-			easing: 'swing',
-			step: function (now) {
-				$(this).text(Math.ceil(now));
-			}
-		});
+				duration: 3000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
 	});
 
 
-	 
-	 
+
+
 	// Menu Trigger
-	$('#menuToggle').on('click', function(event) {
-		var windowWidth = $(window).width();   		 
-		if (windowWidth<1010) { 
-			$('body').removeClass('open'); 
-			if (windowWidth<760){ 
-				$('#left-panel').slideToggle(); 
-			} else {
-				$('#left-panel').toggleClass('open-menu');  
-			} 
-		} else {
-			$('body').toggleClass('open');
-			$('#left-panel').removeClass('open-menu');  
-		} 
-			 
-	}); 
+	// $('#menuToggle').on('click', function (event) {
+	// 	var windowWidth = $(window).width();
+	// 	if (windowWidth < 1010) {
+	// 		$('body').removeClass('open');
+	// 		if (windowWidth < 760) {
+	// 			$('#left-panel').slideToggle();
+	// 		} else {
+	// 			$('#left-panel').toggleClass('open-menu');
+	// 		}
+	// 	} else {
+	// 		$('body').toggleClass('open');
+	// 		$('#left-panel').removeClass('open-menu');
+	// 	}
 
-	 
-	$(".menu-item-has-children.dropdown").each(function() {
-		$(this).on('click', function() {
-			var $temp_text = $(this).children('.dropdown-toggle').html();
-			$(this).children('.sub-menu').prepend('<li class="subtitle">' + $temp_text + '</li>'); 
-		});
-	});
+	// });
+
+
+	// $(".menu-item-has-children.dropdown").each(function () {
+	// 	$(this).on('click', function () {
+	// 		// var $temp_text = $(this).children('.dropdown-toggle').html();
+	// 		// const hasExist = $(this).children('.subtitle');
+	// 		// if (hasExist && hasExist.length === 0) {
+	// 		// 	$(this).children('.sub-menu').prepend('<li class="subtitle">' + $temp_text + '</li>');
+	// 		// }
+	// 	});
+	// });
 
 
 	// Load Resize 
-	$(window).on("load resize", function(event) { 
-		var windowWidth = $(window).width();  		 
-		if (windowWidth<1010) {
-			$('body').addClass('small-device'); 
+	$(window).on("load resize", function (event) {
+		var windowWidth = $(window).width();
+		if (windowWidth < 1010) {
+			$('body').addClass('small-device');
 		} else {
-			$('body').removeClass('small-device');  
-		} 
-		
+			$('body').removeClass('small-device');
+		}
+
 	});
-  
- 
+
+
 });
